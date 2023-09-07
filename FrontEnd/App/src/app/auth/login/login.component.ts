f51a3b9509df659e38201eca79dd055453035ef0
Raysel <141379819+Rayfel2@users.noreply.github.com>
2023-09-07 17:45:23 -0400

    Duro

--- FrontEnd/App/src/app/auth/login/login.component.ts
+++ FrontEnd/App/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,15 +9,25 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class LoginComponent { 
   loginForm=this.fb.group({
-email: ['email.com', [Validators.required, Validators.email]],
+email: ['rayfelogando@gmail.com', [Validators.required, Validators.email]],
 password: ['', [Validators.required]],
   })
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private router: Router) { }
+
+  get email(){
+    return this.loginForm.controls.email;
+  }
+  get password(){
+    return this.loginForm.controls.password;
+  }
 
   login(){
     if (this.loginForm.valid){
       console.log("aqui se llamaria al servicio de login");
+      this.router.navigateByUrl('/inicio');
+      this.loginForm.reset();
     } else {
+      this.loginForm.markAllAsTouched();
       alert ("error al ingresar los datos");
     }
 
