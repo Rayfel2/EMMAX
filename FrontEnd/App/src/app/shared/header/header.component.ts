dec343658b0e1569fcb909071a3c12f6ff2a1810
Raysel <141379819+Rayfel2@users.noreply.github.com>
2023-09-09 01:28:19 -0400

    Dise;o de header y footer
    Falta el responsive y eliminar codigo del CSS del header

--- FrontEnd/App/src/app/shared/header/header.component.ts
+++ FrontEnd/App/src/app/shared/header/header.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/auth/login.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit, OnDestroy {
+  userLoginOn: boolean=false;
+  constructor(private loginService:LoginService) {}
+  logout() {
+   this.loginService.logout();
+   // Puedes agregar cualquier redirección o lógica adicional aquí después de cerrar sesión.
+ }
+ 
+  ngOnDestroy(): void {
+   this.loginService.currentUserData.unsubscribe();
+   this.loginService.currentUserLoginOn.unsubscribe();  
+ }
+ 
+  ngOnInit(): void {
+    this.loginService.currentUserLoginOn.subscribe({
+     next:(userLoginOn) =>{
+       this.userLoginOn= userLoginOn;
+     }
+   });
+ 
+  }
 }
--- FrontEnd/App/src/app/shared/nav/nav.component.ts
+++ FrontEnd/App/src/app/shared/nav/nav.component.ts
@@ -1,29 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { LoginService } from 'src/app/services/auth/login.service';
+
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit, OnDestroy {
- userLoginOn: boolean=false;
- constructor(private loginService:LoginService) {}
- logout() {
-  this.loginService.logout();
-  // Puedes agregar cualquier redirección o lógica adicional aquí después de cerrar sesión.
-}
-
- ngOnDestroy(): void {
-  this.loginService.currentUserData.unsubscribe();
-  this.loginService.currentUserLoginOn.unsubscribe();  
-}
-
- ngOnInit(): void {
-   this.loginService.currentUserLoginOn.subscribe({
-    next:(userLoginOn) =>{
-      this.userLoginOn= userLoginOn;
-    }
-  })
- }
+export class NavComponent {
+ 
 }
