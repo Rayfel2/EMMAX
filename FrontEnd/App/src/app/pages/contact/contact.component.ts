9f3a5ed6d13622afead21999638455b82a2abb41
Raysel <141379819+Rayfel2@users.noreply.github.com>
2023-10-09 18:33:00 -0400

    Listo

--- FrontEnd/App/src/app/pages/contact/contact.component.ts
+++ FrontEnd/App/src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  usuario: string = '';
+  gmail: string = '';
+  comentarios: string = '';
 
+  constructor(private http: HttpClient) {}
+
+  enviarCorreo() {
+    const formData = {
+      usuario: this.usuario,
+      gmail: this.gmail,
+      comentarios: this.comentarios
+    };
+
+    this.http.post('http://localhost:4200/enviar-correo', formData)
+      .subscribe((response: any) => {
+        console.log('Correo enviado con éxito:', response);
+        
+      }, (error) => {
+        console.error('Error al enviar el correo:', error);
+     
+      });
+  }
 }
--- FrontEnd/App/src/app/shared/nav/nav.component.ts
+++ FrontEnd/App/src/app/shared/nav/nav.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -7,5 +7,18 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
- 
+  searchText: string = ''; // Propiedad para almacenar el valor del campo de búsqueda
+
+  constructor(private router: Router) {}
+
+  // Función para redireccionar al componente ShopComponent con el valor de búsqueda
+  searchProducts() {
+    if (this.searchText) {
+      // Redirige a la ruta de ShopComponent y pasa el valor de búsqueda como parámetro
+      this.router.navigate(['/tienda'], { queryParams: { search: this.searchText } });
+    } else {
+      // Si el campo de búsqueda está vacío, redirige a la página de inicio
+      this.router.navigate(['/inicio']);
+    }
+  }
 }
